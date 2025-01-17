3e3dca44adc533a036e9e93c9ae0e7a78b574197
Hsu <tonyhsu2@amd.com>
2025-01-17 11:00:33 +0800

    Refactoring matrixMul.cpp
    - Refactor

--- matrixMul.cpp
+++ matrixMul.cpp
@@ -12,23 +12,26 @@ using namespace std;
 
 #define THREADS_PER_BLOCKDIM 32
 
-
-// #define GRIDDIM1 DIM3
-// #define GRIDDIM2 1
-// #define GRIDDIM3 1
-
 #define RANDSIZE 10
+#define ROWMAJOR 0
+#define COLMAJOR 1
+
+#define ELE_PARTITION  0
+#define TILE_PARTITION 1
 
 
 template<typename T>
 __global__ void matrixMul_rowMajor_tiledKernel(T* A, T* B, T* C, size_t dim_1, size_t dim_2, size_t dim_3) {
+    // Size of shared memory = 2 * size of tile matrix * sizeof(T) (bytes)
+    //                       = 2 * (32*32) * 4 = 8192, for THREADS_PER_BLOCKDIM == 32 and float type.
+    // For MI210, the maximum shared memory per block is 65536 bytes, the allocation of shared memory is valid.
     __shared__ T tileA[THREADS_PER_BLOCKDIM][THREADS_PER_BLOCKDIM];
     __shared__ T tileB[THREADS_PER_BLOCKDIM][THREADS_PER_BLOCKDIM];
-    // Total shared memory = THREADS_PER_BLOCKDIM*THREADS_PER_BLOCKDIM*2
-    // For THREADS_PER_BLOCKDIM == 32 and float data type, Total shared memory = 32*32*2*4 = 8192 < 65536
+
     int i = blockDim.y * blockIdx.y + threadIdx.y;
     int j = blockDim.x * blockIdx.x + threadIdx.x;
     //printf(" blockIdx.y = %d, blockIdx.x = %d, threadIdx.y = %d, threadIdx.x = %d, i = %d, j = %d, blockDim.y = %d, blockDim.x = %d, gridDim.y = %d, gridDim.x = %d\n", blockIdx.y, blockIdx.x, threadIdx.y, threadIdx.x, i, j, blockDim.y, blockDim.x, gridDim.y, gridDim.x);
+
     T sum = 0;
     for (int t = 0; t < ((dim_2+THREADS_PER_BLOCKDIM-1)/THREADS_PER_BLOCKDIM); t++) {
         // Update tileA
@@ -45,7 +48,7 @@ __global__ void matrixMul_rowMajor_tiledKernel(T* A, T* B, T* C, size_t dim_1, s
             tileB[threadIdx.y][threadIdx.x] = 0.0;
         }
 
-        // Wait all threads in the same block have copied the value from global memory.
+        // Wait all threads in the block have copied the value from global memory.
         __syncthreads();
         
         // Calculate tileA*tileB
@@ -63,13 +66,15 @@ __global__ void matrixMul_rowMajor_tiledKernel(T* A, T* B, T* C, size_t dim_1, s
 
 template<typename T>
 __global__ void matrixMul_colMajor_tiledKernel(T* A, T* B, T* C, size_t dim_1, size_t dim_2, size_t dim_3) {
+    // Size of shared memory = 2 * size of tile matrix * sizeof(T) (bytes)
+    //                       = 2 * (32*32) * 4 = 8192, for THREADS_PER_BLOCKDIM == 32 and float type.
+    // For MI210, the maximum shared memory per block is 65536 bytes, the allocation of shared memory is valid.
     __shared__ T tileA[THREADS_PER_BLOCKDIM][THREADS_PER_BLOCKDIM];
     __shared__ T tileB[THREADS_PER_BLOCKDIM][THREADS_PER_BLOCKDIM];
-    // Total shared memory = THREADS_PER_BLOCKDIM*THREADS_PER_BLOCKDIM*2
-    // For THREADS_PER_BLOCKDIM == 32 and float data type, Total shared memory = 32*32*2*4 = 8192 < 65536
     int i = blockDim.y * blockIdx.y + threadIdx.y;
     int j = blockDim.x * blockIdx.x + threadIdx.x;
     //printf(" blockIdx.y = %d, blockIdx.x = %d, threadIdx.y = %d, threadIdx.x = %d, i = %d, j = %d, blockDim.y = %d, blockDim.x = %d, gridDim.y = %d, gridDim.x = %d\n", blockIdx.y, blockIdx.x, threadIdx.y, threadIdx.x, i, j, blockDim.y, blockDim.x, gridDim.y, gridDim.x);
+
     T sum = 0;
     for (int t = 0; t < ((dim_2+THREADS_PER_BLOCKDIM-1)/THREADS_PER_BLOCKDIM); t++) {
         // Update tileA
@@ -105,13 +110,8 @@ __global__ void matrixMul_colMajor_tiledKernel(T* A, T* B, T* C, size_t dim_1, s
 
 template<typename T>
 __global__ void matrixMul_rowMajor_kernel(T* A, T* B, T* C, size_t dim_1, size_t dim_2, size_t dim_3) {
-    //int i = blockDim.x * blockIdx.x + threadIdx.x;          // row
-    //int j = blockDim.y * blockIdx.y + threadIdx.y;          // col
-    // int i = blockIdx.x;           // row
-    // int j = threadIdx.x;          // col
-    int i = blockDim.y * blockIdx.y + threadIdx.y;
-    int j = blockDim.x * blockIdx.x + threadIdx.x;
-    // printf(" i = %d, j = %d\n", i, j);
+    int i = blockDim.y * blockIdx.y + threadIdx.y;      // row
+    int j = blockDim.x * blockIdx.x + threadIdx.x;      // col
     if (i < dim_1  && j < dim_3) {
         T sum = 0;
         for (int k = 0; k < dim_2; k++) {
@@ -123,16 +123,16 @@ __global__ void matrixMul_rowMajor_kernel(T* A, T* B, T* C, size_t dim_1, size_t
 
 template<typename T>
 __global__ void matrixMul_colMajor_kernel(T* A, T* B, T* C, size_t dim_1, size_t dim_2, size_t dim_3) {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;          // row
-    int j = blockDim.y * blockIdx.y + threadIdx.y;          // col
-    // printf(" i = %d, j = %d\n", i, j);
-    T sum = 0;
-    for (int k = 0; k < dim_2; k++) {
-        sum += A[i + k*dim_1] * B[k + j*dim_2];
+    int i = blockDim.y * blockIdx.y + threadIdx.y;          // row
+    int j = blockDim.x * blockIdx.x + threadIdx.x;          // col
+    if (i < dim_1  && j < dim_3) {
+        T sum = 0;
+        for (int k = 0; k < dim_2; k++) {
+            sum += A[i + k*dim_1] * B[k + j*dim_2];
+        }
+        C[i + j*dim_1] = sum;
     }
-    C[i + j*dim_1] = sum;
 }
-
 
 template<typename T>
 void matrixMul_rowMajor_host(T* A, T* B, T* C, size_t dim_1, size_t dim_2, size_t dim_3) {
@@ -158,6 +158,12 @@ void matrixMul_colMajor_host(T* A, T* B, T* C, size_t dim_1, size_t dim_2, size_
             C[i + j*dim_1] = sum;
 	    }
     }
+};
+
+template<typename T>
+void matrixMul_host_handler(T* A, T* B, T* C, size_t dim_1, size_t dim_2, size_t dim_3, int major) {
+    return major == ROWMAJOR ? matrixMul_rowMajor_host(A, B, C, dim_1, dim_2, dim_3) : 
+                               matrixMul_colMajor_host(A, B, C, dim_1, dim_2, dim_3);
 };
 
 template<typename T>
@@ -198,16 +204,39 @@ void compareMatrix(T* mat1, T* mat2, size_t dim_1, size_t dim_2) {
         for(int j = 0; j < dim_2; j++)
         {
             if (mat1[i*dim_2+j] != mat2[i*dim_2+j]) {
-                //cout << "Result difference found at " << i << "," << j << ": " << "CPU: " << mat1[i*dim_2+j] << " GPU: " <<  mat2[i*dim_2+j] << endl;
+                // cout << "Result difference found at " << i << "," << j << ": " << "CPU: " << mat1[i*dim_2+j] << " GPU: " <<  mat2[i*dim_2+j] << endl;
                 num_diff += 1;
             }
             else {
-                //cout << "[" << i << ", " << j << "]: CPU: " << mat1[i*dim_2+j] << ", GPU: " << mat2[i*dim_2+j] << endl;
+                // cout << "[" << i << ", " << j << "]: CPU: " << mat1[i*dim_2+j] << ", GPU: " << mat2[i*dim_2+j] << endl;
             }
         }
     }
     cout << "Number of result differences : " << num_diff << endl;
 }
+
+template<typename T>
+void printAllMatrix(T* A, T* B, T* C, size_t dim_1, size_t dim_2, size_t dim_3, int major) {
+    // Print in row major
+    if (major == ROWMAJOR) {
+        cout << " ----- Row major matrix ----- " << endl;
+        cout << "Matrix A :" << endl;
+        printMatrix_rowMajor(A, dim_1, dim_2);
+        cout << "Matrix B :" << endl;
+        printMatrix_rowMajor(B, dim_2, dim_3);
+        cout << "Matrix C :" << endl;
+        printMatrix_rowMajor(C, dim_1, dim_3);
+    } else {
+        cout << "Column major matrix" << endl;
+        cout << "Matrix A :" << endl;
+        printMatrix_colMajor(A, dim_1, dim_2);
+        cout << "Matrix B :" << endl;
+        printMatrix_colMajor(B, dim_2, dim_3);
+        cout << "Matrix C :" << endl;
+        printMatrix_colMajor(C, dim_1, dim_3);
+    }
+}
+
 
 int main() {
     SCALAR_T* A = new SCALAR_T[DIM1*DIM2];
@@ -224,55 +253,38 @@ int main() {
     initMatrix(B, DIM2, DIM3, RANDSIZE);
     initMatrix(C, DIM1, DIM3, 0);
 
+    std::cout << " --------------- Matrix Size ---------------" << std::endl;
+    cout << "A : (" << DIM1 << ", "<< DIM2 << ")" << endl;
+    cout << "B : (" << DIM2 << ", "<< DIM3 << ")" << endl;
+    cout << "C : (" << DIM1 << ", "<< DIM3 << ")" << endl;
+
     // Matrix Multiplication from Host
-    std::cout << " ---------- CPU ----------" << std::endl;
+    int major = COLMAJOR;
+    int type  = ELE_PARTITION;     // ELE_PARTITION, TILE_PARTITION
+    if (major == ROWMAJOR) cout << "Treat as row major matrix" << endl; else cout << "Treat as column major matrix" << endl;
+    if (type == ELE_PARTITION) cout << "Element wise partitioning..." << endl;
+    else if (type == TILE_PARTITION) cout << "Tile wise partitioning..." << endl;
+
+    std::cout << " --------------- CPU ---------------" << std::endl;
     start = std::chrono::high_resolution_clock::now();
-    matrixMul_rowMajor_host(A, B, C, DIM1, DIM2, DIM3);
+    // matrixMul_rowMajor_host(A, B, C, DIM1, DIM2, DIM3);
+    matrixMul_host_handler(A, B, C, DIM1, DIM2, DIM3, major);
     end = std::chrono::high_resolution_clock::now();
     elapsed = end - start;
     std::cout << "CPU Elapsed time: " << elapsed.count() << " ns" << std::endl;
     std::cout << "Dummy print C[0]: " << C[0] << std::endl;
-    /*
-    // Print in row major
-    cout << "Matrix A :" << endl;
-    printMatrix_rowMajor(A, DIM1, DIM2);
-    cout << "Matrix B :" << endl;
-    printMatrix_rowMajor(B, DIM2, DIM3);
-    cout << "Matrix C :" << endl;
-    printMatrix_rowMajor(C, DIM1, DIM3);
-    */
+    // printAllMatrix(A, B, C, DIM1, DIM2, DIM3, major);
 
-
-    // Matrix Multiplication from Host
-    initMatrix(C, DIM1, DIM3, 0);
-    start = std::chrono::high_resolution_clock::now();
-    matrixMul_colMajor_host(A, B, C, DIM1, DIM2, DIM3);
-    end = std::chrono::high_resolution_clock::now();
-    elapsed = end - start;
-    std::cout << "CPU Elapsed time: " << elapsed.count() << " ns" << std::endl;
-    std::cout << "Dummy print C[0]: " << C[0] << std::endl;
-    /*
-    // Print in column major
-    cout << "Matrix A :" << endl;
-    printMatrix_colMajor(A, DIM1, DIM2);
-    cout << "Matrix B :" << endl;
-    printMatrix_colMajor(B, DIM2, DIM3);
-    cout << "Matrix C :" << endl;
-    printMatrix_colMajor(C, DIM1, DIM3); 
-    */
-    std::cout << " ---------- GPU ----------" << std::endl;
+    std::cout << " --------------- GPU ---------------" << std::endl;
     // hipSetDevice(1);
     int device;
     hipGetDevice(&device);
     std::cout << "Current GPU ID: " << device << std::endl;
 
-
-
-    size_t freeMem, totalMem;
-    hipMemGetInfo(&freeMem, &totalMem);
-    std::cout << "Free Memory: " << freeMem << " bytes" << std::endl;
-    std::cout << "Total Memory: " << totalMem << " bytes" << std::endl;
-
+    // size_t freeMem, totalMem;
+    // hipMemGetInfo(&freeMem, &totalMem);
+    // std::cout << "Free Memory: " << freeMem << " bytes" << std::endl;
+    // std::cout << "Total Memory: " << totalMem << " bytes" << std::endl;
 
     // Create Event Time Mark
     hipEvent_t startHip, stopHip;
@@ -289,13 +301,6 @@ int main() {
     hipMemcpy(d_B, B, DIM2*DIM3*sizeof(SCALAR_T), hipMemcpyHostToDevice);
     hipMemset(d_C, 0, DIM1*DIM3*sizeof(SCALAR_T));
 
-    // Device 0 Information : 
-    // Total Global Memory: 68702699520 bytes
-    // Shared Memory per Block: 65536 bytes
-    // Max Threads per Block: 1024
-    // Max Threads Dimension: [1024, 1024, 1024]
-    // Max Grid Size: [2147483647, 65536, 65536]
-
     dim3 blockDim(THREADS_PER_BLOCKDIM, THREADS_PER_BLOCKDIM);
     dim3 gridDim((DIM3 + THREADS_PER_BLOCKDIM - 1) / THREADS_PER_BLOCKDIM,
                   (DIM1 + THREADS_PER_BLOCKDIM - 1) / THREADS_PER_BLOCKDIM);
@@ -303,10 +308,19 @@ int main() {
     cout << "Grid Dim : (" << gridDim.x << ", " << gridDim.y << ", " << gridDim.z << "), Total # of blocks = " << gridDim.x*gridDim.y*gridDim.z<< endl;
 
     hipEventRecord(startHip, 0);
-    // hipLaunchKernelGGL(matrixMul_rowMajor_kernel, gridDim, blockDim, 0, 0, d_A, d_B, d_C, DIM1, DIM2, DIM3);
-    hipLaunchKernelGGL(matrixMul_rowMajor_tiledKernel, gridDim, blockDim, 0, 0, d_A, d_B, d_C, DIM1, DIM2, DIM3);
-    // hipLaunchKernelGGL(matrixMul_colMajor_tiledKernel, gridDim, blockDim, 0, 0, d_A, d_B, d_C, DIM1, DIM2, DIM3);
-    // hipLaunchKernelGGL(matrixMul_colMajor_kernel, gridDim, blockDim, 0, 0, d_A, d_B, d_C, DIM1, DIM2, DIM3);
+    if (major == ROWMAJOR) {
+        if (type == ELE_PARTITION) {
+            hipLaunchKernelGGL(matrixMul_rowMajor_kernel, gridDim, blockDim, 0, 0, d_A, d_B, d_C, DIM1, DIM2, DIM3);
+        } else if (type == TILE_PARTITION) {
+            hipLaunchKernelGGL(matrixMul_rowMajor_tiledKernel, gridDim, blockDim, 0, 0, d_A, d_B, d_C, DIM1, DIM2, DIM3);
+        }
+    } else {
+        if (type == ELE_PARTITION) {
+            hipLaunchKernelGGL(matrixMul_colMajor_kernel, gridDim, blockDim, 0, 0, d_A, d_B, d_C, DIM1, DIM2, DIM3);
+        } else if (type == TILE_PARTITION) {
+            hipLaunchKernelGGL(matrixMul_colMajor_tiledKernel, gridDim, blockDim, 0, 0, d_A, d_B, d_C, DIM1, DIM2, DIM3);
+        }
+    }
     hipEventRecord(stopHip, 0);
     hipEventSynchronize(stopHip);
     float milliseconds = 0;
